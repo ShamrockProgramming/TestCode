4ecd91507b5a3a5dcd82e1eb74ca34dc7aababa1
Mr. Fleming <efleming@stpatrick.org>
2018-08-28 13:51:30 -0500

    we did it!

---
+++ newthing.js
@@ -0,0 +1,20 @@
+const fruit = "ba";
+const funny = "fruit"*"fruit";
+const A = "a"
+console.log(fruit+funny+A);
+
+/*
+let wage = 15;
+let hours = 30;
+let tax = 0.8;
+let total = wage*(hours*50)*tax;
+console.log(total);
+*/
+
+
+
+/*let var1 = 4;
+let var2 = 66;
+let product = var1*var2;
+console.log(var1+var2+product);
+*/
