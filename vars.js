a01e4201c863f6d515260fc29c79fccd52b2ad68
Mr. Fleming <efleming@stpatrick.org>
2018-08-31 13:53:40 -0500

    donzo

--- vars.js
+++ vars.js
@@ -1,7 +1,8 @@
 let a = 10;
 let b = -25;
 //create two more variables c and d and give them whatever number you want.
-
+let c=2;
+let d=8;
 
 const total = a+b+c+d;
 console.log(total);
@@ -10,15 +11,15 @@ console.log(total);
 
 
 //new section on string variables
-let convo = "hello ";adsfasdfasdf
+let convo = "hello ";
 //set name to your name, first and last.
-let name;
+let name = "jjhgjgh";
 //add string convo and name together.
-const greeting =
+const greeting = "akljsdfhklasdfhasldfk";
 console.log(greeting);
 
 //make your own message similar to what is above.
-let convo2;
-let
-const
-console.log();
+let convo2 = "sup booooyyyyyy";
+let yourname = "koolaid man";
+const ooohhhhyah = convo2+yourname;
+console.log(ooohhhhyah);
